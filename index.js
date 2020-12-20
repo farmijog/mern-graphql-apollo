ddd4b06841310ee466dae799a520c946cd493094
Felipe <felipe.armijo05@hotmail.com>
2020-12-20 02:15:21 -0300

    Register & Login added (types and mutations)

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const { ApolloServer } = require("apollo-server");
-const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 
 const { mongoURL } = require("./config/mdbkey");
-const Post = require("./models/Post");
-
-const typeDefs = gql`
-    type Post{
-        id: ID!,
-        body: String!,
-        createdAt: String!,
-        username: String!
-    }
-    type Query{
-        getPosts: [Post]
-    }
-`
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try {
-                const posts = await Post.find();
-                return posts
-            } catch (error) {
-                throw new Error(error);
-            }
-        }
-    }
-}
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({
     typeDefs,
